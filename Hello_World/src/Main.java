d26344c6ea5b0043ead36c1cfb28d9d73b6935a6
esartium <128316259+esartium@users.noreply.github.com>
2023-09-18 21:56:51 +0300

    Add files via upload

---
+++ Hello_World/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("Hello World !!!!!!!!1"); //после println будет перенос строки
+            System.out.print("Hello World!!!!"); //просто print - никаких переносов
+
+            //Другие способы переноса:
+            System.out.print("Hello\nWorld\n\n\n\nbebebe");
+            System.out.println("\nHelloWorld");
+
+            //Отступ слева:
+            System.out.println("\tHello");
+
+            //Вывести кавычки как текст:
+            System.out.println("bebebe\"\"\"bababa");
+
+            //Вывести / как текст:
+            System.out.println("bbrlbrlb\\blrbllbr");
+
+            /* Комментарий в несколько строк:
+            бабаба
+            бебебе
+            бубубу
+             */
+        }
+    }
